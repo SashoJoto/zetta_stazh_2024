450636c2d413308e3dbc4698b2ed05e03abf1b0a
NikiKerezov <nikolaaleksandrov123@gmail.com>
2024-07-03 13:49:02 +0300

    added recommendation and matching logic

--- server/src/main/java/dev/zettalove/zettalove/ZettaloveApplication.java
+++ server/src/main/java/dev/zettalove/zettalove/ZettaloveApplication.java
@@ -2,8 +2,10 @@ package dev.zettalove.zettalove;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ZettaloveApplication {
 
 	public static void main(String[] args) {
---
+++ server/src/main/java/dev/zettalove/zettalove/controllers/RecommendationController.java
@@ -0,0 +1,25 @@
+package dev.zettalove.zettalove.controllers;
+
+import dev.zettalove.zettalove.entities.user.User;
+import dev.zettalove.zettalove.services.RecommendationService;
+import dev.zettalove.zettalove.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recommendations")
+public class RecommendationController {
+    private final UserService userService;
+    private final RecommendationService recommendationService;
+    @GetMapping("/{id}")
+    public void generateRecommendations(@PathVariable Long id) {
+        Optional<User> user = userService.getUserById(id);
+        user.ifPresent(recommendationService::getRecommendedProfiles);
+    }
+}
